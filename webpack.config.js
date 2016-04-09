532c6018f024ad8c81e609650e147bd47c3138ff
Garrett <graffitisouls@gmail.com>
2016-04-09 19:33:58 -0400

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry:  './src',
+    output: {
+        path:     'builds',
+        filename: 'bundle.js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015']
+          }
+        }
+      ]
+    }
+};
